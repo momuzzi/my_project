6a042c7274836da2adde0db47996388c41e28df2
veronees <jsg6854@naver.com>
2026-02-12 00:05:48 +0900

    feat : db 연결 설정

---
+++ src/main/java/jsg/my_project/rdb/SshDataSourceConfig.java
@@ -0,0 +1,54 @@
+package jsg.my_project.rdb;
+
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SshDataSourceConfig {
+
+    private final SshTunnelConfig initializer;
+
+    @Value("${server}")
+    private String isServer;
+
+    @Value("${spring.cloud.aws.ec2.database_endpoint}")
+    private String databaseEndpoint;
+    @Value("${spring.cloud.aws.ec2.database_port}")
+    private int databasePort;
+
+
+    @Bean("dataSource")
+    @Primary
+    public DataSource dataSource(DataSourceProperties properties) {
+        String url = properties.getUrl().replace("localhost", databaseEndpoint);
+        url = url.replace("[forwardedPort]", String.valueOf(3306));
+        Integer forwardedPort = null;
+
+        if (isServer.equals("false")) {
+            url = properties.getUrl();
+            forwardedPort = initializer.buildSshConnection(databaseEndpoint, databasePort);
+            url = url.replace("[forwardedPort]", String.valueOf(forwardedPort));
+        }
+
+        log.info("Datasource Properties URL: {}", url);
+        log.info("Datasource Properties Username: {}", properties.getUsername());
+        log.info("Datasource Properties Driver ClassName: {}", properties.getDriverClassName());
+
+        log.info(url);
+        return DataSourceBuilder.create()
+            .url(url)
+            .username(properties.getUsername())
+            .password(properties.getPassword())
+            .driverClassName(properties.getDriverClassName())
+            .build();
+    }
+}
---
+++ src/main/java/jsg/my_project/rdb/SshTunnelConfig.java
@@ -0,0 +1,61 @@
+package jsg.my_project.rdb;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SshTunnelConfig {
+
+    @Value("${spring.cloud.aws.ec2.remote_jump_host}")
+    private String remoteJumpHost;
+    @Value("${spring.cloud.aws.ec2.ssh_port}")
+    private int sshPort;
+    @Value("${spring.cloud.aws.ec2.user}")
+    private String user;
+    @Value("${spring.cloud.aws.ec2.private_key_path}")
+    private String privateKeyPath;
+
+    private Session session;
+
+    @PreDestroy
+    public void destroy() {
+        if (session.isConnected()) {
+            session.disconnect();
+        }
+    }
+
+    public Integer buildSshConnection(String endpoint, int port) {
+        Integer forwardPort = null;
+
+        try {
+            log.info("SSH  {}@{}:{}  with {}", user, remoteJumpHost, sshPort, privateKeyPath);
+            JSch jsch = new JSch();
+
+            jsch.addIdentity(privateKeyPath);
+            session = jsch.getSession(user, remoteJumpHost, sshPort);
+            session.setConfig("StrictHostKeyChecking", "no");
+
+            log.info("Starting SSH session connection...");
+            session.connect();
+            log.info("SSH session connected");
+
+            forwardPort = session.setPortForwardingL(0, endpoint, port);
+            log.info("Port forwarding created on local port {} to remote port {}", forwardPort,
+                port);
+        } catch (JSchException e) {
+            log.error(e.getMessage());
+            this.destroy();
+            throw new RuntimeException(e);
+        }
+        return forwardPort;
+    }
+
+
+}
